e05ff7f4445ae0cbd94f5bc11851502163f0ec2e
莫可 <vomisahcivoduycaasi@gmail.com>
2025-08-21 20:23:46 +0800

    Fix path to models

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('/src/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/src/models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('/src/models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('/src/models'),
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
 
 ]).then( 
     startVideo
